6e4816774618a88d474f90010eff55fde1bfd862
Johnson <johnson.peixotoo@gmail.com>
2024-06-21 05:53:40 -0300

    feat: reuniao 20-06

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const documentReady = () => {
   const headerNavMenuLinkList = document.querySelector('.header-nav__menu-link-list');
   const headerNavLinks = [...document.querySelectorAll('.header-nav__menu-link')];
 
-  const sliderTitle = document.querySelector('.slider__title');
+  const sliderTitle = document.querySelector('.slider__container');
   const sliderLearnButton = document.querySelector('.slider__learn-button');
   const sliderStarsImageContainer = document.querySelector('.slider__stars-image-container');
   const sliderMoonImageContainer = document.querySelector('.slider__moon-image-container');
@@ -15,12 +15,12 @@ const documentReady = () => {
   const sliderMountainsFrontImagecontainer = document.querySelector('.slider__mountains-front-image-container');
 
   const documentScroll = () => {
-    header.classList.toggle('header--scroll', window.scrollY > 0);
+    header.classList.toggle('header--scroll', window.scrollY >= 0);
     headerNav.classList.toggle('header-nav--scroll', window.scrollY > 0);
 
     let { scrollY } = window;
     // sliderTitle.style.marginLeft = `${45 - (scrollY * 0.15)}%`;
-    sliderTitle.style.marginBottom = `-${scrollY * 0.55}px`;
+    // sliderTitle.style.marginBottom = `-${(scrollY * 0.55)}%`;
     sliderTitle.style.opacity = 1 - (scrollY * 0.003);
     sliderLearnButton.style.marginTop = `${scrollY}px`;
     sliderStarsImageContainer.style.top = `${scrollY * 0.25}px`;
